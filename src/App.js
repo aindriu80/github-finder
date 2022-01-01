820f102d5d4c70800402b512750bfd650dced265
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-01-01 11:24:32 +0000

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-purple-500">
+      <h1 className="text-xl">Github Finder in React Tailwind and Daisyui</h1>
+    </div>
+  )
+}
+
+export default App
